2c52ed0ae9f0400096f4fdcf5376849c14775bbd
Fernando Nielson <appsons.business@gmail.com>
2022-11-25 08:50:33 +0100

    New Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,27 @@
 import 'package:calculadora/screens/calculator_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic_null_safety/flutter_neumorphic.dart';
 
- 
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return NeumorphicApp(
       debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      home: CalculatorScreen(),
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: Colors.black
+      title: 'Calculadora App',
+      themeMode: ThemeMode.light,
+      theme: NeumorphicThemeData(
+        baseColor: Color(0xFFFFFFFF),
+        lightSource: LightSource.topLeft,
+        depth: 10,
       ),
+      darkTheme: NeumorphicThemeData(
+        baseColor: Color(0xFF3E3E3E),
+        lightSource: LightSource.topLeft,
+        depth: 6,
+      ),
+      home: CalculatorScreen(),
     );
   }
 }
